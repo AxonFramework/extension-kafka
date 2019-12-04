f9c7d6cbf2ff714db08f6c66f4d1ff9446b3badc
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-12-04 15:01:46 +0100

    Write test cases for the SubscribableKafkaMessageSource
    Introduce a SubscribableKafkaMessageSourceTest which asserts the builder
     checks and all provides methods
    
    #17-3

---
+++ kafka/src/test/java/org/axonframework/extensions/kafka/eventhandling/consumer/subscribable/SubscribableKafkaMessageSourceTest.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2010-2019. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.extensions.kafka.eventhandling.consumer.subscribable;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.axonframework.common.AxonConfigurationException;
+import org.axonframework.common.Registration;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.extensions.kafka.eventhandling.consumer.ConsumerFactory;
+import org.axonframework.extensions.kafka.eventhandling.consumer.Fetcher;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.axonframework.extensions.kafka.eventhandling.util.ConsumerConfigUtil.DEFAULT_GROUP_ID;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link SubscribableKafkaMessageSource}, asserting construction and utilization of the class.
+ *
+ * @author Steven van Beelen
+ */
+class SubscribableKafkaMessageSourceTest {
+
+    private static final String TEST_TOPIC = "someTopic";
+    private static final Runnable NO_OP_CLOSE_HANDLER = () -> {
+        // No-op
+    };
+    private static final java.util.function.Consumer<List<? extends EventMessage<?>>> NO_OP_EVENT_PROCESSOR = eventMessages -> {
+        // No-op
+    };
+
+    private ConsumerFactory<String, String> consumerFactory;
+    private Fetcher<EventMessage<?>, String, String> fetcher;
+
+    private SubscribableKafkaMessageSource<String, String> testSubject;
+
+    private Consumer<String, String> mockConsumer;
+
+    @SuppressWarnings("unchecked")
+    @BeforeEach
+    void setUp() {
+        consumerFactory = mock(ConsumerFactory.class);
+        mockConsumer = mock(Consumer.class);
+        when(consumerFactory.createConsumer(DEFAULT_GROUP_ID)).thenReturn(mockConsumer);
+        fetcher = mock(Fetcher.class);
+
+        testSubject = SubscribableKafkaMessageSource.<String, String>builder()
+                .topic(TEST_TOPIC)
+                .groupId(DEFAULT_GROUP_ID)
+                .consumerFactory(consumerFactory)
+                .fetcher(fetcher)
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        testSubject.close();
+    }
+
+    @Test
+    void testBuildWithInvalidTokenThrowsAxonConfigurationException() {
+        assertThrows(AxonConfigurationException.class, () -> SubscribableKafkaMessageSource.builder().topic(null));
+    }
+
+    @Test
+    void testBuildWithInvalidGroupIdThrowsAxonConfigurationException() {
+        assertThrows(AxonConfigurationException.class, () -> SubscribableKafkaMessageSource.builder().groupId(null));
+    }
+
+    @Test
+    void testBuildWithInvalidConsumerFactoryThrowsAxonConfigurationException() {
+        //noinspection unchecked,rawtypes
+        assertThrows(
+                AxonConfigurationException.class,
+                () -> SubscribableKafkaMessageSource.builder().consumerFactory((ConsumerFactory) null)
+        );
+    }
+
+    @Test
+    void testBuildWithInvalidFetcherThrowsAxonConfigurationException() {
+        assertThrows(AxonConfigurationException.class, () -> SubscribableKafkaMessageSource.builder().fetcher(null));
+    }
+
+    @Test
+    void testBuildWithInvalidMessageConverterThrowsAxonConfigurationException() {
+        assertThrows(
+                AxonConfigurationException.class, () -> SubscribableKafkaMessageSource.builder().messageConverter(null)
+        );
+    }
+
+    @Test
+    void testBuildingWhilstMissingRequiredFieldsShouldThrowAxonConfigurationException() {
+        assertThrows(AxonConfigurationException.class, () -> SubscribableKafkaMessageSource.builder().build());
+    }
+
+    @Test
+    void testCancelingSubscribedEventProcessorRunsConnectedCloseHandler() {
+        AtomicBoolean closedEventProcessor = new AtomicBoolean(false);
+        when(fetcher.poll(eq(mockConsumer), any(), any())).thenReturn(() -> closedEventProcessor.set(true));
+
+        Registration registration = testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+        testSubject.start();
+
+        verify(consumerFactory).createConsumer(DEFAULT_GROUP_ID);
+        verify(mockConsumer).subscribe(Collections.singletonList(TEST_TOPIC));
+
+        assertTrue(registration.cancel());
+        assertTrue(closedEventProcessor.get());
+    }
+
+    @Test
+    void testSubscribingTheSameInstanceTwiceDisregardsSecondInstanceOnStart() {
+        when(fetcher.poll(eq(mockConsumer), any(), any())).thenReturn(NO_OP_CLOSE_HANDLER);
+
+        testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+        testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+
+        testSubject.start();
+
+        verify(consumerFactory, times(1)).createConsumer(DEFAULT_GROUP_ID);
+        verify(mockConsumer, times(1)).subscribe(Collections.singletonList(TEST_TOPIC));
+    }
+
+    @Test
+    void testStartSubscribesConsumerToAllProvidedTopics() {
+        when(fetcher.poll(eq(mockConsumer), any(), any())).thenReturn(NO_OP_CLOSE_HANDLER);
+
+        List<String> testTopics = new ArrayList<>();
+        testTopics.add("topicOne");
+        testTopics.add("topicTwo");
+
+        testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+        testSubject.start(testTopics);
+
+        verify(consumerFactory).createConsumer(DEFAULT_GROUP_ID);
+        verify(mockConsumer).subscribe(testTopics);
+    }
+
+    @Test
+    void testStartBuildsConsumersForEverySubscribedEventProcessor() {
+        when(fetcher.poll(eq(mockConsumer), any(), any())).thenReturn(NO_OP_CLOSE_HANDLER);
+
+        java.util.function.Consumer<List<? extends EventMessage<?>>> testEventProcessorOne = eventMessages -> {
+        };
+        testSubject.subscribe(testEventProcessorOne);
+        java.util.function.Consumer<List<? extends EventMessage<?>>> testEventProcessorTwo = eventMessages -> {
+        };
+        testSubject.subscribe(testEventProcessorTwo);
+
+        testSubject.start();
+
+        verify(consumerFactory, times(2)).createConsumer(DEFAULT_GROUP_ID);
+        verify(mockConsumer, times(2)).subscribe(Collections.singletonList(TEST_TOPIC));
+    }
+
+    @Test
+    void testCloseRunsCloseHandlerPerSubscribedEventProcessor() {
+        AtomicBoolean closedEventProcessorOne = new AtomicBoolean(false);
+        AtomicBoolean closedEventProcessorTwo = new AtomicBoolean(false);
+        when(fetcher.poll(eq(mockConsumer), any(), any()))
+                .thenReturn(() -> closedEventProcessorOne.set(true))
+                .thenReturn(() -> closedEventProcessorTwo.set(true));
+
+        java.util.function.Consumer<List<? extends EventMessage<?>>> testEventProcessorOne = eventMessages -> {
+        };
+        testSubject.subscribe(testEventProcessorOne);
+        java.util.function.Consumer<List<? extends EventMessage<?>>> testEventProcessorTwo = eventMessages -> {
+        };
+        testSubject.subscribe(testEventProcessorTwo);
+        testSubject.start();
+
+        testSubject.close();
+
+        verify(consumerFactory, times(2)).createConsumer(DEFAULT_GROUP_ID);
+        verify(mockConsumer, times(2)).subscribe(Collections.singletonList(TEST_TOPIC));
+
+        assertTrue(closedEventProcessorOne.get());
+        assertTrue(closedEventProcessorTwo.get());
+    }
+}
