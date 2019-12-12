98d3beb56dfba48894bd4876febf82f3256cd462
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-12-12 12:21:04 +0100

    Introduce a TrackingTokenConsumerRebalanceListener
    Introduce a ConsumerRebalanceListener tied to the Consumer's subscribe
    call for the streamable solution. This listener will use the assigned
    partitions for the given Consumer and check whether the provided
    KafkaTrackingToken has an offset for them. If yes, it will call
    Consumer$#seek for the provided partition-offset pair. If a new
    partition is assigned to the Consumer, thus the KafkaTrackingToken does
    not know of it, then we forcefully set the offset to zero to start from
    the beginning in that partition. Lastly, to cover bug report #25, we
    will use a Token Supplier instead of a Token directly to cover
    reassignments in a running system. This will replace the Consumerutil
    implementation.
    
    #18 and #25

---
+++ kafka/src/main/java/org/axonframework/extensions/kafka/eventhandling/consumer/streamable/TrackingTokenConsumerRebalanceListener.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2010-2019. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.extensions.kafka.eventhandling.consumer.streamable;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * A {@link ConsumerRebalanceListener} which upon {@link ConsumerRebalanceListener#onPartitionsAssigned(Collection)}
+ * will perform a {@link Consumer#seek(TopicPartition, long)} using the partition offsets in the given {@link
+ * KafkaTrackingToken}.
+ * <p>
+ * This implementation ensures that Axon is in charge of event handling progress by always starting from the beginning
+ * of the stream for unknown partitions. This approach follows how a {@link org.axonframework.eventhandling.TrackingEventProcessor}
+ * deals with unknown progress in any {@link org.axonframework.eventhandling.TrackingToken}.
+ *
+ * @param <K> the key of the records the {@link Consumer} polls
+ * @param <V> the value type of the records the {@link Consumer} polls
+ * @author Steven van Beelen
+ * @since 4.0
+ */
+public class TrackingTokenConsumerRebalanceListener<K, V> implements ConsumerRebalanceListener {
+
+    private final Consumer<K, V> consumer;
+    private final Supplier<KafkaTrackingToken> tokenSupplier;
+
+    /**
+     * Create a {@link ConsumerRebalanceListener} which uses a {@link KafkaTrackingToken} through the given {@code
+     * tokenSupplier} to consciously {@link Consumer#seek(TopicPartition, long)} the offsets of the partitions assigned
+     * to the given {@code consumer}.
+     *
+     * @param consumer      the {@link Consumer} used to call {@link Consumer#seek(TopicPartition, long)} on
+     * @param tokenSupplier the {@link Supplier} of a {@link KafkaTrackingToken}. This should provide the most recent
+     *                      progress of the given {@code consumer}
+     */
+    public TrackingTokenConsumerRebalanceListener(Consumer<K, V> consumer, Supplier<KafkaTrackingToken> tokenSupplier) {
+        this.consumer = consumer;
+        this.tokenSupplier = tokenSupplier;
+    }
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        // Not implemented
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation will use {@link Consumer#seek(TopicPartition, long)} for all given {@code assignedPartitions}
+     * using the offsets known in the {@link KafkaTrackingToken}, retrieved through the {@code tokenSupplier}.
+     * <p>
+     * If no offset is known for a given {@link TopicPartition} then we enforce the offset to {@code 0} to ensure all
+     * known records for the new partition are processed. This could occur if polling is started for the first time or
+     * if the number of partitions for the {@link Consumer}s topic is administratively adjusted.
+     */
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> assignedPartitions) {
+        KafkaTrackingToken currentToken = tokenSupplier.get();
+        assignedPartitions.forEach(assignedPartition -> {
+            int partition = assignedPartition.partition();
+            Map<Integer, Long> tokenPartitionPositions = currentToken.partitionPositions();
+
+            long offset = 0L;
+            if (tokenPartitionPositions.containsKey(partition)) {
+                offset = tokenPartitionPositions.get(partition) + 1;
+            }
+            consumer.seek(assignedPartition, offset);
+        });
+    }
+}
---
+++ kafka/src/test/java/org/axonframework/extensions/kafka/eventhandling/consumer/streamable/TrackingTokenConsumerRebalanceListenerTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2010-2019. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.extensions.kafka.eventhandling.consumer.streamable;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.TopicPartition;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Test cases for the {@link TrackingTokenConsumerRebalanceListener} verifying the {@link Consumer#seek(TopicPartition,
+ * long)} operation is called based on the given {@link KafkaTrackingToken} or defaulted to zero.
+ *
+ * @author Steven van Beelen
+ */
+class TrackingTokenConsumerRebalanceListenerTest {
+
+    public static final String TEST_TOPIC = "some-topic";
+    private final Consumer<?, ?> consumer = mock(Consumer.class);
+
+    @Test
+    void testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeek() {
+        long testOffsetForPartitionZero = 5L;
+        long testOffsetForPartitionOne = 10L;
+        long testOffsetForPartitionTwo = 15L;
+
+        Map<Integer, Long> testPartitionPositions = new HashMap<>();
+        testPartitionPositions.put(0, testOffsetForPartitionZero);
+        testPartitionPositions.put(1, testOffsetForPartitionOne);
+        testPartitionPositions.put(2, testOffsetForPartitionTwo);
+        KafkaTrackingToken testToken = KafkaTrackingToken.newInstance(testPartitionPositions);
+
+        TopicPartition testPartitionZero = new TopicPartition(TEST_TOPIC, 0);
+        TopicPartition testPartitionOne = new TopicPartition(TEST_TOPIC, 1);
+        TopicPartition testPartitionTwo = new TopicPartition(TEST_TOPIC, 2);
+        ArrayList<TopicPartition> testAssignedPartitions = new ArrayList<>();
+        testAssignedPartitions.add(testPartitionZero);
+        testAssignedPartitions.add(testPartitionOne);
+        testAssignedPartitions.add(testPartitionTwo);
+
+        TrackingTokenConsumerRebalanceListener<?, ?> testSubject =
+                new TrackingTokenConsumerRebalanceListener<>(consumer, () -> testToken);
+
+        testSubject.onPartitionsAssigned(testAssignedPartitions);
+
+        // Offset is incremented by one, to proceed with the following record instead of the last one
+        verify(consumer).seek(testPartitionZero, testOffsetForPartitionZero + 1);
+        verify(consumer).seek(testPartitionOne, testOffsetForPartitionOne + 1);
+        verify(consumer).seek(testPartitionTwo, testOffsetForPartitionTwo + 1);
+    }
+
+    @Test
+    void testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeek() {
+        KafkaTrackingToken testToken = KafkaTrackingToken.emptyToken();
+
+        TopicPartition testPartitionZero = new TopicPartition(TEST_TOPIC, 0);
+        TopicPartition testPartitionOne = new TopicPartition(TEST_TOPIC, 1);
+        TopicPartition testPartitionTwo = new TopicPartition(TEST_TOPIC, 2);
+        ArrayList<TopicPartition> testAssignedPartitions = new ArrayList<>();
+        testAssignedPartitions.add(testPartitionZero);
+        testAssignedPartitions.add(testPartitionOne);
+        testAssignedPartitions.add(testPartitionTwo);
+
+        TrackingTokenConsumerRebalanceListener<?, ?> testSubject =
+                new TrackingTokenConsumerRebalanceListener<>(consumer, () -> testToken);
+
+        testSubject.onPartitionsAssigned(testAssignedPartitions);
+
+        // Offset is incremented by one, to proceed with the following record instead of the last one
+        verify(consumer).seek(testPartitionZero, 0);
+        verify(consumer).seek(testPartitionOne, 0);
+        verify(consumer).seek(testPartitionTwo, 0);
+    }
+}
