ed1b466061d88f6606f20fa00fc7185c76dd114e
Ben Kornmeier <bkornmeier@pax8.com>
2023-06-13 12:51:34 -0600

    add tests for pattern subscriber to ConsumerSeekUtils

--- kafka/src/test/java/org/axonframework/extensions/kafka/eventhandling/consumer/ConsumerSeekUtilTest.java
+++ kafka/src/test/java/org/axonframework/extensions/kafka/eventhandling/consumer/ConsumerSeekUtilTest.java
@@ -20,15 +20,18 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
 import org.axonframework.extensions.kafka.eventhandling.consumer.streamable.KafkaTrackingToken;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Test cases for the {@link ConsumerSeekUtil} verifying the {@link Consumer#seek(TopicPartition, long)} operation is
@@ -42,8 +45,7 @@ class ConsumerSeekUtilTest {
     public static final String TEST_TOPIC = "some-topic";
     private final Consumer<?, ?> consumer = mock(Consumer.class);
 
-    @Test
-    void testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeek() {
+    <T extends TopicSubscriber> void _testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeek(T subscriber) {
         long testOffsetForPartitionZero = 5L;
         long testOffsetForPartitionOne = 10L;
         long testOffsetForPartitionTwo = 15L;
@@ -57,22 +59,35 @@ class ConsumerSeekUtilTest {
         TopicPartition testPartitionZero = new TopicPartition(TEST_TOPIC, 0);
         TopicPartition testPartitionOne = new TopicPartition(TEST_TOPIC, 1);
         TopicPartition testPartitionTwo = new TopicPartition(TEST_TOPIC, 2);
-        ArrayList<TopicPartition> testAssignedPartitions = new ArrayList<>();
-        testAssignedPartitions.add(testPartitionZero);
-        testAssignedPartitions.add(testPartitionOne);
-        testAssignedPartitions.add(testPartitionTwo);
-        doReturn(listTopics(testAssignedPartitions)).when(consumer).listTopics();
 
-        ConsumerSeekUtil.seekToCurrentPositions(consumer, () -> testToken, new TopicListSubscriber(Collections.singletonList(TEST_TOPIC)));
+        Map<TopicPartition, Long> testAssignedPartitions = new HashMap<>();
+        testAssignedPartitions.put(testPartitionZero, testOffsetForPartitionZero);
+        testAssignedPartitions.put(testPartitionOne, testOffsetForPartitionOne);
+        testAssignedPartitions.put(testPartitionTwo, testOffsetForPartitionTwo);
+        ArrayList<TopicPartition> testAssignedPartitionList = new ArrayList<>(testAssignedPartitions.keySet());
 
-        // Offset is incremented by one, to proceed with the following record instead of the last one
-        verify(consumer).seek(testPartitionZero, testOffsetForPartitionZero + 1);
-        verify(consumer).seek(testPartitionOne, testOffsetForPartitionOne + 1);
-        verify(consumer).seek(testPartitionTwo, testOffsetForPartitionTwo + 1);
+        doReturn(listTopics(testAssignedPartitionList)).when(consumer).listTopics();
+
+        ConsumerSeekUtil.seekToCurrentPositions(consumer, () -> testToken, subscriber);
+        for (Map.Entry<TopicPartition, Long> entry : testAssignedPartitions.entrySet()) {
+            TopicPartition topicPartition = entry.getKey();
+            Long offset = entry.getValue();
+            verify(consumer).seek(topicPartition, offset + 1);
+        }
+
     }
 
     @Test
-    void testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeek() {
+    void testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeekTopicListSubscriber() {
+        _testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeek(new TopicListSubscriber(Collections.singletonList(TEST_TOPIC)));
+    }
+
+    @Test
+    void testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeekTopicPatternSubscriber() {
+        _testOnPartitionsAssignedUsesTokenOffsetsUponConsumerSeek(new TopicPatternSubscriber(Pattern.compile(TEST_TOPIC)));
+    }
+
+    <T extends TopicSubscriber> void _testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeek(T subscriber) {
         KafkaTrackingToken testToken = KafkaTrackingToken.emptyToken();
 
         TopicPartition testPartitionZero = new TopicPartition(TEST_TOPIC, 0);
@@ -84,12 +99,21 @@ class ConsumerSeekUtilTest {
         testAssignedPartitions.add(testPartitionTwo);
         doReturn(listTopics(testAssignedPartitions)).when(consumer).listTopics();
 
-        ConsumerSeekUtil.seekToCurrentPositions(consumer, () -> testToken, new TopicListSubscriber(Collections.singletonList(TEST_TOPIC)));
+        ConsumerSeekUtil.seekToCurrentPositions(consumer, () -> testToken, subscriber);
 
-        // Offset is incremented by one, to proceed with the following record instead of the last one
         verify(consumer).seek(testPartitionZero, 0);
         verify(consumer).seek(testPartitionOne, 0);
         verify(consumer).seek(testPartitionTwo, 0);
+    }
+
+    @Test
+    void testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeekUsingListSubscriber() {
+        _testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeek(new TopicListSubscriber(Collections.singletonList(TEST_TOPIC)));
+    }
+
+    @Test
+    void testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeekUsingPatternSubscriber() {
+        _testOnPartitionsAssignedUsesOffsetsOfZeroForEmptyTokenUponConsumerSeek(new TopicPatternSubscriber(Pattern.compile(TEST_TOPIC)));
     }
 
     private Map<String, List<PartitionInfo>> listTopics(List<TopicPartition> partitions) {
