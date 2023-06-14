317215795d76598c156e7dbc5be249dd411f5e3f
Ben Kornmeier <bkornmeier@pax8.com>
2023-06-14 13:18:43 -0600

    checkout ParameterizedTest work in SubscribableKafkaMessageSourceTest also pass verification through parameters

--- kafka/src/test/java/org/axonframework/extensions/kafka/eventhandling/consumer/subscribable/SubscribableKafkaMessageSourceTest.java
+++ kafka/src/test/java/org/axonframework/extensions/kafka/eventhandling/consumer/subscribable/SubscribableKafkaMessageSourceTest.java
@@ -24,8 +24,12 @@ import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.extensions.kafka.eventhandling.consumer.AsyncFetcher;
 import org.axonframework.extensions.kafka.eventhandling.consumer.ConsumerFactory;
 import org.axonframework.extensions.kafka.eventhandling.consumer.Fetcher;
-import org.axonframework.extensions.kafka.eventhandling.consumer.TopicSubscriberBuilder;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.lang.reflect.Field;
 import java.time.Duration;
@@ -34,13 +38,24 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
 import static org.awaitility.Awaitility.await;
 import static org.axonframework.extensions.kafka.eventhandling.util.ConsumerConfigUtil.DEFAULT_GROUP_ID;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests for {@link SubscribableKafkaMessageSource}, asserting construction and utilization of the class.
@@ -58,6 +73,8 @@ class SubscribableKafkaMessageSourceTest {
         // No-op
     };
 
+    private static final Pattern TEST_PATTERN = Pattern.compile(TEST_TOPIC);
+
     private ConsumerFactory<String, String> consumerFactory;
     private Fetcher<String, String, EventMessage<?>> fetcher;
 
@@ -65,107 +82,22 @@ class SubscribableKafkaMessageSourceTest {
 
     private Consumer<String, String> mockConsumer;
 
-    abstract class SubscribableKafkaMessageSourceTestFixture {
-        abstract <T extends TopicSubscriberBuilder<T>> T setTopic(TopicSubscriberBuilder<T> builder);
-        void testStartBuildsConsumersUpToConsumerCount(int expectedNumberOfConsumers) {
-            when(fetcher.poll(eq(mockConsumer), any(), any(), any())).thenReturn(NO_OP_FETCHER_REGISTRATION);
 
-            SubscribableKafkaMessageSource<String, String> testSubject =
-                    setTopic(SubscribableKafkaMessageSource.<String, String>builder())
-                            .groupId(DEFAULT_GROUP_ID)
-                            .consumerFactory(consumerFactory)
-                            .fetcher(fetcher)
-                            .consumerCount(expectedNumberOfConsumers)
-                            .build();
-
-            testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
-            testSubject.start();
-
-            verify(consumerFactory, times(expectedNumberOfConsumers)).createConsumer(DEFAULT_GROUP_ID);
-            verify(fetcher, times(expectedNumberOfConsumers)).poll(eq(mockConsumer), any(), any(), any());
-        }
-
-        void testCloseRunsCloseHandlerPerConsumerCount(int expectedNumberOfConsumers) {
-
-            AtomicBoolean closedEventProcessorOne = new AtomicBoolean(false);
-            AtomicBoolean closedEventProcessorTwo = new AtomicBoolean(false);
-            when(fetcher.poll(eq(mockConsumer), any(), any(), any()))
-                    .thenReturn(() -> {
-                        closedEventProcessorOne.set(true);
-                        return true;
-                    })
-                    .thenReturn(() -> {
-                        closedEventProcessorTwo.set(true);
-                        return true;
-                    });
-
-            SubscribableKafkaMessageSource<String, String> testSubject =
-                    setTopic(SubscribableKafkaMessageSource.<String, String>builder())
-                            .groupId(DEFAULT_GROUP_ID)
-                            .consumerFactory(consumerFactory)
-                            .fetcher(fetcher)
-                            .autoStart()
-                            .consumerCount(expectedNumberOfConsumers)
-                            .build();
-
-            testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
-            testSubject.close();
-
-            verify(consumerFactory, times(expectedNumberOfConsumers)).createConsumer(DEFAULT_GROUP_ID);
-            verify(fetcher, times(expectedNumberOfConsumers)).poll(eq(mockConsumer), any(), any(), any());
-
-            assertTrue(closedEventProcessorOne.get());
-            assertTrue(closedEventProcessorTwo.get());
-        }
-
-        void restartingConsumerShouldNotCauseAMemoryLeakAndOnCloseNoRegistrationsShouldBeleft() throws NoSuchFieldException, IllegalAccessException {
-            fetcher = AsyncFetcher.<String, String, EventMessage<?>>builder()
-                    .executorService(newSingleThreadExecutor()).build();
-            when(mockConsumer.poll(any(Duration.class))).thenThrow(new BrokerNotAvailableException("none available"));
-
-            SubscribableKafkaMessageSource<String, String> testSubject =
-                    setTopic(SubscribableKafkaMessageSource.<String, String>builder())
-                            .groupId(DEFAULT_GROUP_ID)
-                            .consumerFactory(consumerFactory)
-                            .fetcher(fetcher)
-                            .autoStart()
-                            .build();
-
-            testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
-
-            await().atMost(Duration.ofSeconds(4)).untilAsserted(() -> verify(consumerFactory, atLeast(4)).createConsumer(DEFAULT_GROUP_ID));
-            Field fetcherRegistrations = SubscribableKafkaMessageSource.class.getDeclaredField("fetcherRegistrations");
-
-            fetcherRegistrations.setAccessible(true);
-
-            Map<Integer, Registration> registrations = (Map<Integer, Registration>) fetcherRegistrations.get(testSubject);
-            assertEquals(1, registrations.values().size());
-
-            testSubject.close();
-
-            registrations = (Map<Integer, Registration>) fetcherRegistrations.get(testSubject);
-            assertTrue(registrations.isEmpty());
-        }
-    }
-
-    class ListSubscribableKafkaMessageSourceTestFixture extends SubscribableKafkaMessageSourceTestFixture {
-        @Override
-        <T extends TopicSubscriberBuilder<T>> T setTopic(TopicSubscriberBuilder<T> builder) {
-            return builder.topics(Collections.singletonList(TEST_TOPIC));
-        }
-    }
-
-    class PatternSubscribableKafkaMessageSourceTestFixture extends SubscribableKafkaMessageSourceTestFixture {
-        final private Pattern pattern;
-
-        PatternSubscribableKafkaMessageSourceTestFixture(Pattern pattern) {
-            this.pattern = pattern;
-
-        }
-        @Override
-        <T extends TopicSubscriberBuilder<T>> T setTopic(TopicSubscriberBuilder<T> builder) {
-            return builder.topicPattern(pattern);
-        }
+    private static Stream<Arguments> getSourceBuilders() {
+        return Stream.of(
+                Arguments.of(SubscribableKafkaMessageSource.<String, String>builder().topics(Collections.singletonList(TEST_TOPIC)),
+                        (BiFunction<Integer, Consumer<String, String>, Void>) (consumerCount, mockConsumer) -> {
+                            verify(mockConsumer, times(consumerCount)).subscribe(Collections.singletonList(TEST_TOPIC));
+                            return null;
+                        }
+                ),
+                Arguments.of(SubscribableKafkaMessageSource.<String, String>builder().topicPattern(TEST_PATTERN),
+                        (BiFunction<Integer, Consumer<String, String>, Void>) (consumerCount, mockConsumer) -> {
+                            verify(mockConsumer, times(consumerCount)).subscribe(TEST_PATTERN);
+                            return null;
+                        }
+                )
+        );
     }
 
     @SuppressWarnings("unchecked")
@@ -177,11 +109,11 @@ class SubscribableKafkaMessageSourceTest {
         fetcher = mock(Fetcher.class);
 
         testSubject = SubscribableKafkaMessageSource.<String, String>builder()
-                                                    .topics(Collections.singletonList(TEST_TOPIC))
-                                                    .groupId(DEFAULT_GROUP_ID)
-                                                    .consumerFactory(consumerFactory)
-                                                    .fetcher(fetcher)
-                                                    .build();
+                .topics(Collections.singletonList(TEST_TOPIC))
+                .groupId(DEFAULT_GROUP_ID)
+                .consumerFactory(consumerFactory)
+                .fetcher(fetcher)
+                .build();
     }
 
     @AfterEach
@@ -247,12 +179,12 @@ class SubscribableKafkaMessageSourceTest {
 
         SubscribableKafkaMessageSource<String, String> testSubject =
                 SubscribableKafkaMessageSource.<String, String>builder()
-                                              .topics(Collections.singletonList(TEST_TOPIC))
-                                              .groupId(DEFAULT_GROUP_ID)
-                                              .consumerFactory(consumerFactory)
-                                              .fetcher(fetcher)
-                                              .autoStart()
-                                              .build();
+                        .topics(Collections.singletonList(TEST_TOPIC))
+                        .groupId(DEFAULT_GROUP_ID)
+                        .consumerFactory(consumerFactory)
+                        .fetcher(fetcher)
+                        .autoStart()
+                        .build();
 
         testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
 
@@ -271,12 +203,12 @@ class SubscribableKafkaMessageSourceTest {
 
         SubscribableKafkaMessageSource<String, String> testSubject =
                 SubscribableKafkaMessageSource.<String, String>builder()
-                                              .topics(Collections.singletonList(TEST_TOPIC))
-                                              .groupId(DEFAULT_GROUP_ID)
-                                              .consumerFactory(consumerFactory)
-                                              .fetcher(fetcher)
-                                              .autoStart() // This enables auto close
-                                              .build();
+                        .topics(Collections.singletonList(TEST_TOPIC))
+                        .groupId(DEFAULT_GROUP_ID)
+                        .consumerFactory(consumerFactory)
+                        .fetcher(fetcher)
+                        .autoStart() // This enables auto close
+                        .build();
 
         Registration registration = testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
         testSubject.start();
@@ -312,11 +244,11 @@ class SubscribableKafkaMessageSourceTest {
 
         SubscribableKafkaMessageSource<String, String> testSubject =
                 SubscribableKafkaMessageSource.<String, String>builder()
-                                              .topics(testTopics)
-                                              .groupId(DEFAULT_GROUP_ID)
-                                              .consumerFactory(consumerFactory)
-                                              .fetcher(fetcher)
-                                              .build();
+                        .topics(testTopics)
+                        .groupId(DEFAULT_GROUP_ID)
+                        .consumerFactory(consumerFactory)
+                        .fetcher(fetcher)
+                        .build();
 
         testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
         testSubject.start();
@@ -326,43 +258,95 @@ class SubscribableKafkaMessageSourceTest {
         verify(fetcher).poll(eq(mockConsumer), any(), any(), any());
     }
 
-    @Test
-    void testStartBuildsConsumersUpToConsumerCountUsingListBasedSubscription() {
+    @ParameterizedTest()
+    @MethodSource("getSourceBuilders")
+    void testStartBuildsConsumersUpToConsumerCount(SubscribableKafkaMessageSource.Builder<String, String> builder, BiFunction<Integer, Consumer<String, String>, Void> verifyMock) {
         int expectedNumberOfConsumers = 2;
-        new ListSubscribableKafkaMessageSourceTestFixture().testStartBuildsConsumersUpToConsumerCount(expectedNumberOfConsumers);
-        verify(mockConsumer, times(expectedNumberOfConsumers)).subscribe(Collections.singletonList(TEST_TOPIC));
+
+        when(fetcher.poll(eq(mockConsumer), any(), any(), any())).thenReturn(NO_OP_FETCHER_REGISTRATION);
+
+        SubscribableKafkaMessageSource<String, String> testSubject =
+                builder
+                        .groupId(DEFAULT_GROUP_ID)
+                        .consumerFactory(consumerFactory)
+                        .fetcher(fetcher)
+                        .consumerCount(expectedNumberOfConsumers)
+                        .build();
+
+        testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+        testSubject.start();
+
+        verify(consumerFactory, times(expectedNumberOfConsumers)).createConsumer(DEFAULT_GROUP_ID);
+        verifyMock.apply(expectedNumberOfConsumers, mockConsumer);
+        verify(fetcher, times(expectedNumberOfConsumers)).poll(eq(mockConsumer), any(), any(), any());
     }
 
-    @Test
-    void testCloseRunsCloseHandlerPerConsumerCountUsingListBasedSubscription() {
+    @ParameterizedTest()
+    @MethodSource("getSourceBuilders")
+    void testCloseRunsCloseHandlerPerConsumerCount(SubscribableKafkaMessageSource.Builder<String, String> builder, BiFunction<Integer, Consumer<String, String>, Void> verifyMock) {
         int expectedNumberOfConsumers = 2;
-        new ListSubscribableKafkaMessageSourceTestFixture().testCloseRunsCloseHandlerPerConsumerCount(expectedNumberOfConsumers);
-        verify(mockConsumer, times(expectedNumberOfConsumers)).subscribe(Collections.singletonList(TEST_TOPIC));
+
+        AtomicBoolean closedEventProcessorOne = new AtomicBoolean(false);
+        AtomicBoolean closedEventProcessorTwo = new AtomicBoolean(false);
+        when(fetcher.poll(eq(mockConsumer), any(), any(), any()))
+                .thenReturn(() -> {
+                    closedEventProcessorOne.set(true);
+                    return true;
+                })
+                .thenReturn(() -> {
+                    closedEventProcessorTwo.set(true);
+                    return true;
+                });
+
+        SubscribableKafkaMessageSource<String, String> testSubject =
+                builder
+                        .groupId(DEFAULT_GROUP_ID)
+                        .consumerFactory(consumerFactory)
+                        .fetcher(fetcher)
+                        .autoStart()
+                        .consumerCount(expectedNumberOfConsumers)
+                        .build();
+
+        testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+        testSubject.close();
+
+        verify(consumerFactory, times(expectedNumberOfConsumers)).createConsumer(DEFAULT_GROUP_ID);
+        verifyMock.apply(expectedNumberOfConsumers, mockConsumer);
+        verify(fetcher, times(expectedNumberOfConsumers)).poll(eq(mockConsumer), any(), any(), any());
+
+        assertTrue(closedEventProcessorOne.get());
+        assertTrue(closedEventProcessorTwo.get());
     }
 
-    @Test
-    void restartingConsumerShouldNotCauseAMemoryLeakAndOnCloseNoRegistrationsShouldBeleftUsingListBasedSubscription() throws NoSuchFieldException, IllegalAccessException {
-        new ListSubscribableKafkaMessageSourceTestFixture().restartingConsumerShouldNotCauseAMemoryLeakAndOnCloseNoRegistrationsShouldBeleft();
-    }
-    @Test
-    void testStartBuildsConsumersUpToConsumerCountUsingPatternBasedSubscription() {
-        int expectedNumberOfConsumers = 2;
-        Pattern pattern = Pattern.compile(TEST_TOPIC);
-        new PatternSubscribableKafkaMessageSourceTestFixture(pattern).testStartBuildsConsumersUpToConsumerCount(expectedNumberOfConsumers);
-        verify(mockConsumer, times(expectedNumberOfConsumers)).subscribe(pattern);
-    }
 
-    @Test
-    void testCloseRunsCloseHandlerPerConsumerCountUsingPatternBasedSubscription() {
-        int expectedNumberOfConsumers = 2;
-        Pattern pattern = Pattern.compile(TEST_TOPIC);
-        new PatternSubscribableKafkaMessageSourceTestFixture(pattern).testCloseRunsCloseHandlerPerConsumerCount(expectedNumberOfConsumers);
-        verify(mockConsumer, times(expectedNumberOfConsumers)).subscribe(pattern);
-    }
+    @ParameterizedTest()
+    @MethodSource("getSourceBuilders")
+    void restartingConsumerShouldNotCauseAMemoryLeakAndOnCloseNoRegistrationsShouldBeleft(SubscribableKafkaMessageSource.Builder<String, String> builder, BiFunction<Integer, Consumer<String, String>, Void> verifyMock) throws NoSuchFieldException, IllegalAccessException {
+        fetcher = AsyncFetcher.<String, String, EventMessage<?>>builder()
+                .executorService(newSingleThreadExecutor()).build();
+        when(mockConsumer.poll(any(Duration.class))).thenThrow(new BrokerNotAvailableException("none available"));
 
-    @Test
-    void restartingConsumerShouldNotCauseAMemoryLeakAndOnCloseNoRegistrationsShouldBeleftUsingPatternBasedSubscription() throws NoSuchFieldException, IllegalAccessException {
-        Pattern pattern = Pattern.compile(TEST_TOPIC);
-        new PatternSubscribableKafkaMessageSourceTestFixture(pattern).restartingConsumerShouldNotCauseAMemoryLeakAndOnCloseNoRegistrationsShouldBeleft();
+        SubscribableKafkaMessageSource<String, String> testSubject =
+                builder
+                        .groupId(DEFAULT_GROUP_ID)
+                        .consumerFactory(consumerFactory)
+                        .fetcher(fetcher)
+                        .autoStart()
+                        .build();
+
+        testSubject.subscribe(NO_OP_EVENT_PROCESSOR);
+
+        await().atMost(Duration.ofSeconds(4)).untilAsserted(() -> verify(consumerFactory, atLeast(4)).createConsumer(DEFAULT_GROUP_ID));
+        Field fetcherRegistrations = SubscribableKafkaMessageSource.class.getDeclaredField("fetcherRegistrations");
+
+        fetcherRegistrations.setAccessible(true);
+
+        Map<Integer, Registration> registrations = (Map<Integer, Registration>) fetcherRegistrations.get(testSubject);
+        assertEquals(1, registrations.values().size());
+
+        testSubject.close();
+
+        registrations = (Map<Integer, Registration>) fetcherRegistrations.get(testSubject);
+        assertTrue(registrations.isEmpty());
     }
 }
