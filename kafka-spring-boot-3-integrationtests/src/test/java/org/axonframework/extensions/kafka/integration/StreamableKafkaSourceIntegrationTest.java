479d15ecf347d90ce96fb7a9b620d6a20f15f442
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-02-24 11:49:03 +0100

    Add Kafka Listener environment variable
    Add Kafka Listener environment variable. Apparently newer versions of Kafka do not work nicely with Test Containers yet without the KAFKA_LISTENERS environment variable. Uncovered this solution through a GitHub issue with testcontainers -> https://github.com/testcontainers/testcontainers-java/issues/9506
    Although the referred-to issue is from November 2024, the issue lies with Kafka, which hasn't solved it yet.
    
    #552

--- kafka-spring-boot-3-integrationtests/src/test/java/org/axonframework/extensions/kafka/integration/StreamableKafkaSourceIntegrationTest.java
+++ kafka-spring-boot-3-integrationtests/src/test/java/org/axonframework/extensions/kafka/integration/StreamableKafkaSourceIntegrationTest.java
@@ -41,7 +41,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class StreamableKafkaSourceIntegrationTest {
 
     @Container
-    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer("apache/kafka");
+    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer("apache/kafka-native")
+            .withEnv("KAFKA_LISTENERS", "PLAINTEXT://:9092,BROKER://:9093,CONTROLLER://:9094");
 
     private ApplicationContextRunner testApplicationContext;
 
